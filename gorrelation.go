3ed1a015bb85f5ad28392b59a43eaee9ce07852e
Trevor Oke <trevor@trevoroke.com>
2018-08-19 21:52:01 -0400

    initial commit

--- gorrelation.go
+++ gorrelation.go
@@ -1,7 +1,34 @@
 package gorrelation
 
-import "fmt"
+import (
+	"github.com/satori/go.uuid"
+	"log"
+	"net/http"
+)
 
-func main() {
-	fmt.Println("vim-go")
+type Gorrelation struct {
+	header string
+}
+
+func New() *Gorrelation {
+	gorrelation := &Gorrelation{
+		header: "Correlation-Id",
+	}
+
+	return gorrelation
+}
+
+func (gr *Gorrelation) Handler(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if len(r.Header.Get(gr.header)) == 0 {
+			correlationId, err := uuid.NewV4()
+			if err != nil {
+				log.Printf("could not assign correlation id: %+v", err)
+			}
+
+			r.Header.Add(gr.header, correlationId.String())
+		}
+
+		h.ServeHTTP(w, r)
+	})
 }
---
+++ gorrelation_test.go
@@ -0,0 +1,59 @@
+package gorrelation
+
+import (
+	_ "fmt"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestAddCorrelationId(t *testing.T) {
+	g := New()
+
+	res := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "http://example.com/test", nil)
+
+	g.Handler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, 36, len(r.Header.Get(g.header)), "should return UUID length of 36")
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})).ServeHTTP(res, req)
+
+	assert.Equal(t, res.Code, http.StatusOK)
+}
+
+func TestCorrelationIdExists(t *testing.T) {
+	g := New()
+
+	res := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "http://example.com/test", nil)
+	req.Header.Add(g.header, "example")
+
+	g.Handler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "example", r.Header.Get(g.header))
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})).ServeHTTP(res, req)
+
+	assert.Equal(t, res.Code, http.StatusOK)
+}
+
+func TestCorrelationIdHeaderDifferent(t *testing.T) {
+	g := New()
+	g.header = "Some-Other-Header"
+
+	res := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "http://example.com/test", nil)
+
+	g.Handler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, 36, len(r.Header.Get(g.header)), "should return UUID length of 36")
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})).ServeHTTP(res, req)
+
+	assert.Equal(t, res.Code, http.StatusOK)
+}
